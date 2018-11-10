6cafa5d7ba5a450c280ba2d85f78081528c0c26d
Patrick Wu <magetron1@gmail.com>
2018-11-10 00:36:42 +0000

    some tweaks on the interface desgin

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'custom')));
 console.log('Server Started at Port 4000');
 
 app.use('/', index);
